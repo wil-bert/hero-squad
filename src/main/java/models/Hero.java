29f68da08fe08b8cd513f31bace9a4b4ba3abe47
wil-bert <ianwilbuts@gmail.com>
2020-09-12 18:43:14 +0300

    update hero class

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,45 @@
 package models;
 
+import javax.swing.text.html.HTMLWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
 public class Hero {
+    private String name;
+    private  int age;
+    private String power;
+    private String weakness;
+    private int id;
+    private static ArrayList<Hero> instances = new ArrayList<>();
+
+    public Hero(String name, Integer age, String power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    public String getName() {return this.name;}
+    public int getAge() {return this.age;}
+    public String getPower() {return this.power;}
+    public String getWeakness() {return this.weakness;}
+    public static ArrayList<Hero> getAllInstances() {return instances;}
+    public static void clearAllHeroes(){instances.clear();}
+    public int getId(){return id;}
+    public static Hero findById(int id) {return instances.get(id-1);}
+
+    public static Hero setUpNewHero(){
+        return new Hero("Anko",20, "Flying", "Anger");
+    }
+    public static Hero setUpNewHero1(){
+        return new Hero("Muhari",25,"Stretching","Fire");
+    }
+    public static Hero setUpNewHero2(){
+        return new Hero("Isokah",27,"Speed","Cold");
+    }
+
+
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,14 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
 
+    @Test
+    public void newHero_instantiates_true() {
+        Hero newHero = Hero.setUpNewHero();
+        assertTrue(newHero instanceof Hero);
+    }
 }
