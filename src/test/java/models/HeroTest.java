9464de67baf67ffb6bcb4793992d93b972ec91dc
wil-bert <ianwilbuts@gmail.com>
2020-09-12 18:49:23 +0300

    hero tests work

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -11,4 +11,46 @@ public class HeroTest {
         Hero newHero = Hero.setUpNewHero();
         assertTrue(newHero instanceof Hero);
     }
+    @Test
+    public void newHero_getName_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("Anko",newHero.getName());
+    }
+    @Test
+    public void newHero_getAge_Int() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals(20,newHero.getAge());
+    }
+    @Test
+    public void newHero_getPower_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("Flying",newHero.getPower());
+    }
+    @Test
+    public void newHero_getWeakness_String() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals("Anger",newHero.getWeakness());
+    }
+    @Test
+    public void newHero_getAllInstances_true() {
+        Hero newHero = Hero.setUpNewHero();
+        Hero another = Hero.setUpNewHero();
+        assertTrue(Hero.getAllInstances().contains(newHero));
+        assertTrue(Hero.getAllInstances().contains(another));
+    }
+    @Test
+    public void newHero_getId_Int() {
+        Hero.clearAllHeroes();
+        Hero newHero = Hero.setUpNewHero();
+        Hero another = Hero.setUpNewHero();
+        Hero another1 = Hero.setUpNewHero();
+        assertEquals(3,another1.getId());
+    }
+    @Test
+    public void newHero_findById_id() {
+        Hero.clearAllHeroes();
+        Hero newHero = Hero.setUpNewHero();
+        Hero another = Hero.setUpNewHero();
+        assertEquals(2,Hero.findById(another.getId()).getId());
+    }
 }
